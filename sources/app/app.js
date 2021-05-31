5f04d1f94918db150a66c3c0d3045a3783006b32
Nhan <vinh.yan93@gmail.com>
2021-05-31 19:48:49 -0400

    add discount cost function

--- sources/app/app.js
+++ sources/app/app.js
@@ -2,11 +2,16 @@ const views = document.querySelector('.views');
 const slider = document.querySelector('.slider');
 const pageviews = [10000, 50000, 100000, 500000, 1000000];
 const cost = document.querySelector('.cost');
-const priceRange = [8, 12, 16, 24, 36];
+const yearlyPriceArr = [8, 12, 16, 24, 36];
 const toggle = document.querySelector('.toggle');
 let yearlyBilling = document.querySelector('.yearly-billing');
+const toggleBtn = document.querySelector('#toggle-btn');
+let monthlyPriceArr;
+const noDiscountCost = document.querySelector('.no-discount-cost');
 
 
+
+// Pageview number formatter: 
 
 const numFormatter = (num) => {
     if(num > 999 && num < 1000000){
@@ -18,6 +23,8 @@ const numFormatter = (num) => {
     }
 }
 
+// Currency formatter:
+
 const currencyFormatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -25,39 +32,6 @@ const currencyFormatter = new Intl.NumberFormat('en-US', {
 
 
 const app = () => {
-
-    // Pageviews and Price range
-
-    slider.addEventListener('input', (e) => {
-      
-        const sliderValue = e.target.value;
-
-        switch (sliderValue) {
-            case '0':
-                views.innerHTML = numFormatter(pageviews[0]);
-                cost.innerHTML = currencyFormatter.format(priceRange[0]);
-                break;
-            case '1':
-                views.innerHTML = numFormatter(pageviews[1]);
-                cost.innerHTML = currencyFormatter.format(priceRange[1]);
-                break;
-            case '2':
-                views.innerHTML = numFormatter(pageviews[2]);
-                cost.innerHTML = currencyFormatter.format(priceRange[2]);
-                break;
-            case '3':
-                views.innerHTML = numFormatter(pageviews[3]);
-                cost.innerHTML = currencyFormatter.format(priceRange[3]);
-                break;
-            default:
-                views.innerHTML = numFormatter(pageviews[4]);
-                cost.innerHTML = currencyFormatter.format(priceRange[4]);
-        }
-  
-    });
-
-
-
 
     // Slider color changing for desktop:
 
@@ -79,21 +53,67 @@ const app = () => {
         slider.style.background = color;
     });
 
+    // Monthly and yearly billing switch function:
 
-    // Toggle Yearly Billing Discount:
+    const showViewsPrice = (arr, typeOfCost) => {
 
-    toggle.addEventListener('click', () => {
+        slider.addEventListener('input', (e) => {
+      
+            const sliderValue = e.target.value;
+    
+            switch (sliderValue) {
+                case '0':
+                    views.innerHTML = numFormatter(pageviews[0]);
+                    typeOfCost.innerHTML = currencyFormatter.format(arr[0]);
+                    break;
+                case '1':
+                    views.innerHTML = numFormatter(pageviews[1]);
+                    typeOfCost.innerHTML = currencyFormatter.format(arr[1]);
+                    break;
+                case '2':
+                    views.innerHTML = numFormatter(pageviews[2]);
+                    typeOfCost.innerHTML = currencyFormatter.format(arr[2]);
+                    break;
+                case '3':
+                    views.innerHTML = numFormatter(pageviews[3]);
+                    typeOfCost.innerHTML = currencyFormatter.format(arr[3]);
+                    break;
+                default:
+                    views.innerHTML = numFormatter(pageviews[4]);
+                    typeOfCost.innerHTML = currencyFormatter.format(arr[4]);
+            }
+      
+        });
 
+    };
+    
+    
+    showViewsPrice(yearlyPriceArr, cost);
+
+
+    // Toggle Monthly billing price (without discount):
+
+    toggleBtn.addEventListener('change', (e) => {
+     
         yearlyBilling.classList.toggle("toggle-discount");
+
+        if (toggleBtn.checked == true) {
+            monthlyPriceArr = yearlyPriceArr.map(price => {
+                return price / 0.75;
+            });
+
+            noDiscountCost.style.display = "initial";
+            cost.style.display = "none";
+
+            showViewsPrice(monthlyPriceArr, noDiscountCost);
+
+        } else {
+            showViewsPrice(yearlyPriceArr);
+            noDiscountCost.style.display = "none";
+            cost.style.display = "initial";
+        }
+
     });
-
-
-
-
-
-
-
-
 
 };
 
